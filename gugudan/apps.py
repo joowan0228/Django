5b8c640aed49345eadd5b14be56a310a338671e8
DESKTOP-5JIBLGH\pat76 <pat7654@naver.com>
2025-09-15 17:28:54 +0900

    Remove tracked files to apply .gitignore

--- gugudan/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GugudanConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gugudan'
--- movies/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def movie_list(request):
-    return HttpResponse("<h1>🎬 영화 페이지</h1><p>여기에 영화 목록이 표시됩니다.</p>")
